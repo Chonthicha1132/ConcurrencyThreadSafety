c8d4169f8a753ca86836dbbbb8d945b67c822ca9
Chonthicha1132 <chonthicha.tae@ku.th>
2026-09-04 16:49:12 +0700

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -45,8 +45,14 @@ public class Bank {
         //
         // ห้ามแก้ด้วยการเอาล็อกใบใดใบหนึ่งออก — ยอดรวมจะเพี้ยน
         // ---------------------------------------------------------------
-        synchronized (from) {
-            synchronized (to) {
+Account frist = from;
+Account second = to;
+if (from.id()>to.id()) {
+    frist = to ;
+    second = from ;
+}
+        synchronized (frist) {
+            synchronized (second) {
                 if (!from.withdraw(amount)) {
                     return false;
                 }
